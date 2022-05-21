ef19875214e5f0d67c49be884feb3234fe3dd73a
Peter Sterckx <pasterckx@gmail.com>
2022-05-21 10:42:09 -0400

    Initial commit

--- lib/backend2-stack.ts
+++ lib/backend2-stack.ts
@@ -1,16 +1,71 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {
+  Stack,
+  StackProps,
+  aws_lambda as lambda,
+  aws_iam as iam,
+} from "aws-cdk-lib";
+import { WebSocketLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import { WebSocketApi, WebSocketStage } from "@aws-cdk/aws-apigatewayv2-alpha";
+import { Construct } from "constructs";
 
 export class Backend2Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const wsHanlderRole = new iam.Role(this, "WSHandlerRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Backend2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const wsHandler = new lambda.Function(this, "WSHandler", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "ws-handler.handler",
+      role: wsHanlderRole,
+    });
+
+    wsHanlderRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AWSLambdaBasicExecutionRole"
+      )
+    );
+    wsHanlderRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "AmazonAPIGatewayInvokeFullAccess"
+      )
+    );
+
+    const webSocketApi = new WebSocketApi(this, "mywsapi", {
+      connectRouteOptions: {
+        integration: new WebSocketLambdaIntegration(
+          "ConnectIntegration",
+          wsHandler
+        ),
+      },
+      disconnectRouteOptions: {
+        integration: new WebSocketLambdaIntegration(
+          "DisconnectIntegration",
+          wsHandler
+        ),
+      },
+      defaultRouteOptions: {
+        integration: new WebSocketLambdaIntegration(
+          "DefaultIntegration",
+          wsHandler
+        ),
+      },
+    });
+
+    webSocketApi.addRoute("broadcastMessage", {
+      integration: new WebSocketLambdaIntegration(
+        "BroadcastMessageIntegration",
+        wsHandler
+      ),
+    });
+
+    const webSocketStage = new WebSocketStage(this, "mywsstage", {
+      webSocketApi,
+      stageName: "dev",
+      autoDeploy: true,
+    });
   }
 }
